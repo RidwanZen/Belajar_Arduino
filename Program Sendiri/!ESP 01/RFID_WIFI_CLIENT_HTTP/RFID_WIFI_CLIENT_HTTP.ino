d73df0483a1ee319f02004287835f7f7d956d2a3
Muhammad Ridwan Ramadhan <86788917+RidwanZen@users.noreply.github.com>
2021-12-13 07:20:43 +0700

    Add files via upload

--- Program Sendiri/!ESP 01/RFID_WIFI_CLIENT_HTTP/RFID_WIFI_CLIENT_HTTP.ino
+++ Program Sendiri/!ESP 01/RFID_WIFI_CLIENT_HTTP/RFID_WIFI_CLIENT_HTTP.ino
@@ -3,7 +3,7 @@
 #include <SoftwareSerial.h>
 #include <SPI.h>
 #include <MFRC522.h>
-
+/*
 //INISIALISASI LCD
 #include <LiquidCrystal_I2C.h>  //LIBRARY LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2); //Inisialisasi LCD
@@ -12,14 +12,14 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); //Inisialisasi LCD
 #include <Servo.h>  //LIBRARY SERVO
 int outServo=5; 
 Servo servo;
-
+*/
 SoftwareSerial wifiSerial(2, 3); // RX, TX
 
 char ssid[]        = "04FS_2c9560";            // your network SSID (name)
 char pass[]        = "Rahasia01";        // your network password
 int    HTTP_PORT   = 80;
 String HTTP_METHOD = "GET";
-char   HOST_NAME[] = "192.168.1.13"; // change to your PC's IP address
+char   HOST_NAME[] = "192.168.1.11"; // change to your PC's IP address
 String PATH_NAME   = "/rfidui/proses.php";
 String getData;
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
@@ -35,13 +35,12 @@ int merah=7;
 int kunci=0;
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   wifiSerial.begin(9600);
   WiFi.init(&wifiSerial);
-  lcd.begin();
-  servo.attach(outServo);
+  //lcd.begin();
+  //servo.attach(outServo);
   pinMode(merah,OUTPUT);
   pinMode(buzzer,OUTPUT);
   pinMode(IR,INPUT);
@@ -71,6 +70,10 @@ void setup()
   // if you get a connection, report back via serial
   if (client.connect(HOST_NAME, HTTP_PORT)) {
     Serial.println("Berhasil terhubung ke Server!");
+    //lcd.clear();
+    //lcd.setCursor(0,0); lcd.print("TEMPELKAN KARTU");
+    //servo.write(90);
+    delay(1000);
   }
 }
 
@@ -95,8 +98,12 @@ void loop()
   Serial.print("Message : ");
   uidString.toUpperCase();
   Serial.println(uidString);
+  //lcd.clear();
+  //lcd.setCursor(0,0); lcd.print("UID : " + String(uidString));
+  digitalWrite(merah,HIGH);
   digitalWrite(buzzer,HIGH);
   delay(100);
+  digitalWrite(merah,LOW);
   digitalWrite(buzzer,LOW);
 
   //POST TO WEB
@@ -119,7 +126,7 @@ void loop()
         getData.trim();
         
         //AMBIL DATA JSON
-        const size_t capacity = JSON_OBJECT_SIZE(9) + 144; //cari dulu nilainya pakai Arduino Json 5 Asisten
+        const size_t capacity = JSON_OBJECT_SIZE(13) + 200; //cari dulu nilainya pakai Arduino Json 5 Asisten
         DynamicJsonDocument doc(capacity);
         //StaticJsonDocument<192> doc;
         DeserializationError error = deserializeJson(doc, getData);
@@ -131,11 +138,12 @@ void loop()
         const char* saldoakhir      = doc["saldo_akhir"]; 
         const char* namatol_dibaca  = doc["namatol"];
         const char* tanggal_dibaca  = doc["tanggal"];
-        const char* status_dibaca  = doc["status_bayar"];
+        const char* status_dibaca   = doc["status_bayar"];
         
 
        //LOGIKA
        if(String(rfid_dibaca)!=""){
+        kunci=1;
         buzzeroke();
         Serial.println("Kartu Terdaftar!");
         Serial.println(getData);
@@ -150,9 +158,15 @@ void loop()
          Serial.print("Waktu        = ");Serial.println(tanggal_dibaca);
          Serial.println(status_dibaca);
          Serial.println();
-       }else{
-        buzzergagal();
-        Serial.println("Kartu Tidak terdaftar!");
+       } /*else if(bacaIR==0 && kunci==1){
+            kunci=2;
+       } else if(bacaIR==1 && kunci==2){
+            kunci=0;
+            tutup();
+       } */else{
+            kunci=0;
+            buzzergagal();
+            Serial.println("Kartu Tidak terdaftar!");
        }
       } 
     }
@@ -161,6 +175,7 @@ void loop()
 
 
 void buzzeroke(){
+  //lcd.setCursor(0,1); lcd.print("KARTU TERDAFTAR");
   digitalWrite(buzzer,HIGH);
   delay(100);
   digitalWrite(buzzer,LOW);
@@ -169,11 +184,44 @@ void buzzeroke(){
   delay(100);
   digitalWrite(buzzer,LOW);
   delay(100);
+  //lcd.clear();
+  Serial.println("PALANG DIBUKA");
+  //lcd.setCursor(2,0); lcd.print("PALANG DIBUKA");
+  //servo.write(0);
+  delay(1000);
+  //lcd.setCursor(1,1); lcd.print("SILAHKAN MASUK");
 }
 
 void buzzergagal(){
-  digitalWrite(buzzer,HIGH);
+  /*lcd.clear();
+  lcd.setCursor(3,0); lcd.print("MOHON MAAF");
+  lcd.setCursor(0,1); lcd.print("KARTU ANDA GAGAL");
+  */digitalWrite(buzzer,HIGH);
   delay(1000);
   digitalWrite(buzzer,LOW);
   delay(10);
+  //lcd.clear();
+  //lcd.setCursor(0,0); lcd.print("TEMPELKAN KARTU");
 }
+
+/*void tutup(){
+  Serial.println("PALANG DITUTUP");
+  lcd.clear();
+  lcd.setCursor(1,0); lcd.print("PALANG DITUTUP");
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  digitalWrite(buzzer,LOW);
+  delay(100);
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  digitalWrite(buzzer,LOW);
+  servo.write(90);
+  delay(1000);
+  Serial.println("TERIMAKASIH");
+  lcd.setCursor(2,1); lcd.print("TERIMAKASIH");
+  delay(1000);
+  lcd.clear();
+  Serial.println("TEMPELKAN KARTU");
+  lcd.setCursor(0,0); lcd.print("TEMPELKAN KARTU");
+  delay(1000);
+}*/
